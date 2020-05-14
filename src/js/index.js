b9a40bebf4c3a25b483afc8c690b695647d3ef29
Rino Nakagawara <nakagawara@reclick.co.jp>
2020-05-14 13:09:39 +0900

    global navの左側サムネイルの実装（アニメーション追加）

--- src/js/index.js
+++ src/js/index.js
@@ -23,6 +23,28 @@
 //   });
 // });
 
+var _ua = (function (u) {
+  return {
+    Tablet:
+      (u.indexOf("windows") != -1 &&
+        u.indexOf("touch") != -1 &&
+        u.indexOf("tablet pc") == -1) ||
+      u.indexOf("ipad") != -1 ||
+      (u.indexOf("android") != -1 && u.indexOf("mobile") == -1) ||
+      (u.indexOf("firefox") != -1 && u.indexOf("tablet") != -1) ||
+      u.indexOf("kindle") != -1 ||
+      u.indexOf("silk") != -1 ||
+      u.indexOf("playbook") != -1,
+    Mobile:
+      (u.indexOf("windows") != -1 && u.indexOf("phone") != -1) ||
+      u.indexOf("iphone") != -1 ||
+      u.indexOf("ipod") != -1 ||
+      (u.indexOf("android") != -1 && u.indexOf("mobile") != -1) ||
+      (u.indexOf("firefox") != -1 && u.indexOf("mobile") != -1) ||
+      u.indexOf("blackberry") != -1,
+  };
+})(window.navigator.userAgent.toLowerCase());
+
 //　オープニング
 $(function () {
   setTimeout(function () {
@@ -56,31 +78,53 @@ $(function () {
   }
 
   // サムネイル取得
-  $(".l-gnav-body__list--item").on("mouseover touchend", function () {
+  var dataStylePrev01 = "message01";
+  var dataStylePrev02 = "message02";
+
+  $(".js-gnav-list").on("mouseover touchend", function () {
     var dataUrl01 = $(this).attr("data-url-01");
     var dataUrl02 = $(this).attr("data-url-02");
     var dataStyle01 = $(this).attr("data-style-01");
     var dataStyle02 = $(this).attr("data-style-02");
-    $("#main-thumb-01").attr({ src: dataUrl01, class: dataStyle01 });
-    $("#main-thumb-02").attr({ src: dataUrl02, class: dataStyle02 });
+
+    if (dataStyle01 !== dataStylePrev01) {
+      //現在表示されているメニューと異なる場合
+      $("#main-thumb-01").fadeOut(300, function () {
+        $("#main-thumb-01").attr({ src: dataUrl01 }).fadeIn(300);
+        $("#main-box-01").attr({ class: dataStyle01 });
+        dataStylePrev01 = dataStyle01;
+      });
+    }
+    if (dataStyle02 !== dataStylePrev02) {
+      //現在表示されているメニューと異なる場合
+      $("#main-thumb-02").fadeOut(300, function () {
+        $("#main-thumb-02").attr({ src: dataUrl02 }).fadeIn(300);
+        $("#main-box-02").attr({ class: dataStyle02 });
+        dataStylePrev02 = dataStyle02;
+      });
+    }
   });
 
-  // //#titleを取得
+  // サムネイル移動
   let thumb01 = document.getElementById("main-thumb-01");
   let thumb02 = document.getElementById("main-thumb-02");
 
   document.addEventListener("mousemove", (event) => {
     //X座標(値は適宜調整)
-    var x = Math.round((event.pageX / 100) * 10);
+    var x1 = Math.round((event.pageX / 100) * 8);
     //Y座標(値は適宜調整)
-    var y = Math.round((event.pageY / 100) * 10);
+    var y1 = Math.round((event.pageY / 100) * 8);
+    //X座標(値は適宜調整)
+    var x2 = Math.round((event.pageX / 100) * 5);
+    //Y座標(値は適宜調整)
+    var y2 = Math.round((event.pageY / 100) * 5);
 
     //#titleのX軸・Y軸を設定
-    thumb01.style.left = x + "px";
-    thumb01.style.top = y + "px";
+    thumb01.style.left = -x1 + "px";
+    thumb01.style.top = -y1 + "px";
 
-    thumb02.style.left = -x + "px";
-    thumb02.style.top = -y + "px";
+    thumb02.style.left = -x2 + "px";
+    thumb02.style.top = -y2 + "px";
   });
 
   // セレクトボックス
@@ -102,8 +146,10 @@ $(function () {
     if ($(".l-gnav").hasClass("on")) {
       //fullnavが開いている時
       $(".l-gnav").fadeOut();
-      $(".l-btn-open, .l-header__logo--off").addClass("on");
-      $(".l-btn-close, .l-header__logo--on").removeClass("on");
+      if (!_ua.Mobile && !_ua.Tablet) {
+        $(".l-btn-open, .l-header__logo--off").addClass("on");
+        $(".l-btn-close, .l-header__logo--on").removeClass("on");
+      }
       setTimeout(function () {
         $(".l-gnav").removeClass("on");
       }, 500);
@@ -113,22 +159,25 @@ $(function () {
       setTimeout(function () {
         $(".l-gnav").addClass("on");
       }, 200);
-      $(".l-btn-close, .l-header__logo--on").addClass("on");
-      $(".l-btn-open, .l-header__logo--off").removeClass("on");
+      if (!_ua.Mobile && !_ua.Tablet) {
+        $(".l-btn-close, .l-header__logo--on").addClass("on");
+        $(".l-btn-open, .l-header__logo--off").removeClass("on");
+      }
     }
   });
 
   // logo表示/非表示
-  var startPos = 0,
-    winScrollTop = 0;
+  winScrollTop = 0;
   $(window).on("scroll", function () {
-    winScrollTop = $(this).scrollTop();
-    if (winScrollTop >= 50) {
-      $(".l-header__logo").addClass("hide");
-    } else {
-      $(".l-header__logo").removeClass("hide");
+    if (!$(".l-gnav").hasClass("on")) {
+      winScrollTop = $(this).scrollTop();
+      if (winScrollTop >= 50) {
+        $(".l-header__logo").addClass("hide");
+      } else {
+        $(".l-header__logo").removeClass("hide");
+      }
+      startPos = winScrollTop;
     }
-    startPos = winScrollTop;
   });
 });
 
